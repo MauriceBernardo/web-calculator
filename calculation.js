0c082de09e75abbce3445a9ab13958cfa541c268
MauriceBernardo <Maurice_tikus@hotmail.com>
2018-11-28 19:21:11 +0700

    Added functionality to the calculator

--- calculation.js
+++ calculation.js
@@ -1,8 +1,101 @@
-var result = document.querySelector('.result');
+var storedValue = 0;
+var screen = document.querySelector(".result");
+var result = "0";
+var lastOperand = null;
 
+init();
 
+function init(){
+    document.querySelector(".calculator").addEventListener("click", 
+    function(event) {
+        handleClick(event.target.innerText);
+    });
+}
 
-document.querySelector('.main-number').addEventListener
-('click', function(event) {
-    result = event.target.innerText;  
-})
+function handleClick(text){
+    if(isNaN(parseInt(text))){
+        handleSymbol(text);
+    } else {
+        handleNumber(text);
+    }
+
+    rerender();
+}
+
+function handleSymbol(symbol){
+    switch (symbol) {
+        case "C":
+            result = "0";
+            storedValue = 0;
+            break;
+    
+        case "←":
+            if (result.length == 1) {
+                result = "0";
+            } else {
+                result = result.substring(0,result.length-1);
+            }
+            break;
+
+        case "=":
+            if(lastOperand === null){
+                result = "0";
+            }
+            applyMath(lastOperand);
+            lastOperand = null;
+            result = storedValue;
+            storedValue = 0;
+            break;
+        
+        case "+":
+        case "-":
+        case "x":
+        case "÷":
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleNumber(number){
+    if(result === "0"){
+        result = number;
+    } else {
+        result += number;
+    }
+}
+
+function handleMath(operand){
+    if(parseInt(result) === 0){
+        result = "0";
+    }
+
+    if(storedValue === 0){
+        storedValue = parseInt(result);
+    } else {
+        applyMath(operand);
+    }
+
+    lastOperand = operand;
+    result = "0";
+}
+
+function applyMath(operand){
+    switch (operand) {
+        case "+":
+            storedValue += parseInt(result);
+            break;
+        case "-":
+            storedValue -= parseInt(result);
+            break;
+        case "x":
+            storedValue *= parseInt(result);
+            break;
+        case "÷":
+            storedValue /= parseInt(result);
+            break;
+    }
+}
+
+function rerender() {
+    screen.innerText = result;
+} 
