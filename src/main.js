befd19dfd871a9a1d5d40e0469a8a3b02c918921
MauriceBernardo <Maurice_tikus@hotmail.com>
2018-12-12 09:39:44 +0700

    deleted redundant file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Calculator from './components/Calculator.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(Calculator),
 }).$mount('#app')
